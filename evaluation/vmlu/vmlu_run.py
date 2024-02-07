900b1f068bc837155505cfb9be0b330270c0537d
Xuan-Phi Nguyen <nxphi47@gmail.com>
2024-02-07 10:06:49 +0800

    Update vmlu_run.py

--- evaluation/vmlu/vmlu_run.py
+++ evaluation/vmlu/vmlu_run.py
@@ -5,7 +5,7 @@ from vllm import LLM, SamplingParams
 import csv
 
 jsonl_path = f"test.jsonl"
-out_csv_path = f"pred2.csv"
+out_csv_path = f"vmlu_pred.csv"
 
 def read_json(json_file):
     print(f'Reading : {json_file}')
@@ -18,15 +18,12 @@ questions = read_json(jsonl_path)
 
 sampling_params = SamplingParams(temperature=0.0, max_tokens=1, stop=["</s>"])
 model_path = "SeaLLMs/SeaLLM-7B-v2"
-# model_path = "/mnt/workspace/workgroup/phi/pret_models/models--SeaLLMs--SeaLLM-7B-v2/snapshots/06c960c2b8fb75d453533c281fe31f550a04c61a"
 model = LLM(model_path, dtype="bfloat16")
 
-
+# previous commit incorrect put a \n between </s> & <|im_start|>, there should not be any \n between </s> and <|im_start|>
 CHAT_TEMPLATE = """<|im_start|>system
-You are a helpful assistant.</s>
-<|im_start|>user
-{prompt}</s>
-<|im_start|>assistant
+You are a helpful assistant.</s><|im_start|>user
+{prompt}</s><|im_start|>assistant
 """
 question_template = """Chỉ đưa ra chữ cái đứng trước câu trả lời đúng (A, B, C, D hoặc E) của câu hỏi trắc nghiệm sau:
 
@@ -45,6 +42,8 @@ def to_prompt(item):
 
 prompts = [to_prompt(q) for q in questions]
 
+print(prompts[0])
+
 generated = model.generate(prompts, sampling_params)
 responses = [g.outputs[0].text for g in generated]
 answers = [r.strip()[0] for r in responses]
@@ -59,4 +58,4 @@ with open(out_csv_path, 'w', encoding='utf-8') as f:
         _ = write.writerow([q['id'], pred])
 
 
-
+print(f"prediction saved at {out_csv_path}")
